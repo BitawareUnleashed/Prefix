3a7805aa33f2a122cf8d2e96c115791e91eb206a
BitawareUnleashed <dario.benevento@gmail.com>
2024-12-27 11:18:46 +0100

    Add project files.

---
+++ Prefix/Program.cs
@@ -0,0 +1,36 @@
+﻿var ts = DateTime.Now;
+Console.WriteLine(Solution.LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+
+var tt= DateTime.Now - ts;
+
+Console.WriteLine("ns: " + tt.TotalMilliseconds);
+
+Console.ReadLine();
+
+
+public static class Solution
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+            return string.Empty;
+
+        int minLength = int.MaxValue;
+        foreach (var str in strs)
+        {
+            if (str.Length < minLength) minLength = str.Length;
+        }
+
+        for (int i = 0; i < minLength; i++)
+        {
+            char currentChar = strs[0][i];
+            foreach (var str in strs)
+            {
+                if (str[i] != currentChar)
+                    return strs[0].Substring(0, i);
+            }
+        }
+
+        return strs[0].Substring(0, minLength);
+    }
+}
